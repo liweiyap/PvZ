a89bb12032a2b306dbe7c74ffa8c985f53624aa4
liweiyap <liweiyap@gmail.com>
2019-11-03 16:35:12 +0100

    Add new test.

---
+++ tests/hello-world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 12:28:37 2019
+
+@author: leewayleaf
+"""
+
+def main():
+    print("Hello world")
+
+# -------------------------------------- #
+
+if __name__ == "__main__":
+    main()
